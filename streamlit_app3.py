e97a797eb538de25225dc78b1f259d96990dbb60
miwa370 <hashimoto.miwa.5063@members.co.jp>
2024-11-27 16:51:41 +0900

    Update streamlit_app3.py

--- streamlit_app3.py
+++ streamlit_app3.py
@@ -8,7 +8,7 @@ st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 st.write(
     """Choose the fruits you want in your custom Smoothie!
     """)
-import requests
+import pandas as pd
 
 
 
